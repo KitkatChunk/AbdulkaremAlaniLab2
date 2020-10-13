8208730ba226b8d3b173f3a004c611d674e749d5
Karem <karemalani2402@yahoo.com>
2020-10-13 02:07:38 -0400

    Added second page

---
+++ AbdulkaremLab2/app/src/main/java/com/example/abdulkarem/Alani/S300993768/AbdulkaremHomeType.java
@@ -0,0 +1,14 @@
+package com.example.abdulkarem.Alani.S300993768;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AbdulkaremHomeType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abdulkarem_home_type);
+    }
+}
--- AbdulkaremLab2/app/src/main/java/com/example/abdulkarem/Alani/S300993768/AbdulkaremMainActivity.java
+++ AbdulkaremLab2/app/src/main/java/com/example/abdulkarem/Alani/S300993768/AbdulkaremMainActivity.java
@@ -2,13 +2,16 @@ package com.example.abdulkarem.Alani.S300993768;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AbdulkaremMainActivity extends AppCompatActivity {
 
 
-
+    private Button btnMove;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,19 @@ public class AbdulkaremMainActivity extends AppCompatActivity {
         ImageView myImageView = (ImageView) findViewById(R.id.AlaniImageview);
         myImageView.setImageResource(R.drawable.renthome);
 
+        btnMove = findViewById(R.id.AlaniStartButton);
+
+        btnMove.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                moveToHomeType();
+            }
+        });
+
+    }
+
+    private void moveToHomeType(){
+        Intent intent = new Intent(AbdulkaremMainActivity.this, AbdulkaremHomeType.class);
+        startActivity(intent);
     }
 }
